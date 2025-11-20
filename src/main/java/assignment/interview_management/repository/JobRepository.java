a1de988278efcc36e91b3b2d8b1b1277aa39e9d0
hathanh <dothanhha2910@gmail.com>
2025-11-20 16:24:11 +0700

    Update logic for job service

--- src/main/java/assignment/interview_management/repository/JobRepository.java
+++ src/main/java/assignment/interview_management/repository/JobRepository.java
@@ -2,6 +2,7 @@ package assignment.interview_management.repository;
 
 import assignment.interview_management.dto.GetAllJobQuery;
 import assignment.interview_management.dto.GetJobSkillQuery;
+import assignment.interview_management.dto.JobsForInterviewQuery;
 import assignment.interview_management.entity.Job;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -31,4 +32,11 @@ public interface JobRepository extends JpaRepository<Job, Long> {
             "FROM job " +
             "WHERE (UPPER(title) LIKE UPPER(CONCAT('%', :search, '%'))) ", nativeQuery = true)
     Integer countJob(@Param("search") String search);
+
+    @Query(value = "SELECT id, title " +
+            "FROM job WHERE status = 'OPEN' " +
+            "UNION ALL " +
+            "SELECT id, title " +
+            "FROM job WHERE id = (SELECT job_id FROM interview WHERE id = :id) ", nativeQuery = true)
+    List<JobsForInterviewQuery> findAllJob(@Param("id") Long id);
 }
